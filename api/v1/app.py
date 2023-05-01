34494430abb1a6d68997aff64e7898c948f591dc
SylviaJitti <sylviajitti@gmail.com>
2023-05-01 19:45:45 +0000

    HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,7 +12,7 @@ from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(aap, resources={r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
